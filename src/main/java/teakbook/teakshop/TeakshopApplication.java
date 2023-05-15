68bd0d3db409e2ef8306a4d5892ea423c69f26be
yongtaek <laundryday@naver.com>
2023-05-15 22:12:30 +0900

    First

---
+++ src/main/java/teakbook/teakshop/TeakshopApplication.java
@@ -0,0 +1,13 @@
+package teakbook.teakshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeakshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeakshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teakbook/teakshop/TeakshopApplicationTests.java
@@ -0,0 +1,13 @@
+package teakbook.teakshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeakshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
